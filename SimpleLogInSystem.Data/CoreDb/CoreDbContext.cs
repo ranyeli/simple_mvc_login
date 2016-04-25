c180145d13696b6e1951c4e11a73291e58b44795
ranyeli <robert.ranyeli@gmail.com>
2016-04-24 20:38:42 -0800

    add data project

---
+++ SimpleLogInSystem.Data/CoreDb/CoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SimpleLogInSystem.Data.CoreDb.Entities;
+
+namespace SimpleLogInSystem.Data.CoreDb
+{
+    public class CoreDbContext:DbContext
+    {
+        DbSet<User> Users { get; set; }
+    }
+}
